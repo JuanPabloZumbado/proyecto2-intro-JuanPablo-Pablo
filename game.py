744a7acbb3d188b00b4c7f149d5967a20c9d4e8f
JuanPabloZumbado <juanpablo.z.cit@gmail.com>
2025-11-27 07:20:20 -0600

    Commit prueba

--- game.py
+++ game.py
@@ -358,8 +358,6 @@ while running:
                     for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             running = False
-
-                        if
                         
 
                         if event.type == pygame.KEYDOWN:
